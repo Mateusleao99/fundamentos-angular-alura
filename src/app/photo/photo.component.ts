ac3b05e5570939eceb81d0ec760b3ccbb9a55cf7
MateusLeao99 <mateus.san.tos@hotmail.com>
2020-11-30 17:08:17 -0300

    feat: Adicionando bootstrap e criando primeiro componente

---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-photo',
+  templateUrl: './photo.component.html',
+  styleUrls: ['./photo.component.css']
+})
+export class PhotoComponent {
+  description= 'leao';
+
+  url = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg"
+
+}
