baa29ff7d43adb03e745cf4635bf1c9359ae7279
MateusLeao99 <mateus.san.tos@hotmail.com>
2021-01-06 16:21:47 -0300

    feat: criando tela login e funcao logout

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,8 @@
+import { Observable } from 'rxjs';
+import { UserService } from './../user/user.service';
 import { Component } from "@angular/core";
+import { User } from '../user/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ap-header',
@@ -6,4 +10,14 @@ import { Component } from "@angular/core";
 })
 export class HeaderComponent {
 
+  user$: Observable<User>;
+
+  constructor(private userService: UserService, private router:Router){
+    this.user$ = userService.getUser();
+  }
+
+  logout(){
+    this.userService.logout();
+    this.router.navigate(['']);
+  }
 }
